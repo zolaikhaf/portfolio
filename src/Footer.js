59b57a7f632362dab39054813752fba5aaa41375
Zolaikha Afzal Freidah <zolaikha.afzal@gmail.com>
2024-03-27 17:16:53 -0400

    footer enhancements

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,10 @@ const Footer = () => {
 
   return (
 
-      <div className="footer" style={{position: "relative", background: "#000", color: "#fff", padding: "5em", zIndex: "10"}}><h2>Thanks for Visiting :)</h2></div>
+      <div className="footer" style={{position: "relative", background: "#000", color: "#fff", padding: "5em", zIndex: "10"}}>
+            <h2>Thanks for Visiting :)</h2>
+            <p>Built with <a href="https://github.com/zolaikhaf/portfolio?tab=readme-ov-file#thanks-for-visiting-my-site" target="_blank">React</a></p>
+        </div>
 
   );
 };
