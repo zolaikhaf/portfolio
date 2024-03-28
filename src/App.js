9572cac270f407594f7acee828a4fe0d0dc3a24a
Zolaikha Afzal Freidah <zolaikha.afzal@gmail.com>
2024-03-28 16:04:09 -0400

    loader added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import React, { useEffect } from "react";
+
 import './App.css';
 import './BootstrapOverrides.css';
 
+import Spinner from "./Spinner.js";
 import Header from "./Header.js";
 import Body from "./Body.js";
 import Footer from "./Footer.js";
 
 function App() {
 
-  return (
+  const [isLoading, setIsLoading] = React.useState(true);
 
+  const handleLoading = () => {
+  setIsLoading(false);
+  }
+
+  useEffect(()=>{
+    window.addEventListener("load",handleLoading);
+    return () => window.removeEventListener("load",handleLoading);
+  },[])
+
+  return !isLoading ? (
     <div className="App">
 
       <Header />
@@ -19,7 +32,8 @@ function App() {
 
     </div>
 
-  );
+  ):(<Spinner />)
+
 }
 
 export default App;
